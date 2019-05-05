64f594eea1f234692b387ed6570e7bb3a2126d9b
Robert D. Thurston <rdt@redoubt.io>
2019-05-04 16:09:18 -0800

    Update default.rb

--- chef/demo-cookbook/recipes/default.rb
+++ chef/demo-cookbook/recipes/default.rb
@@ -5,6 +5,30 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 # Start Service and configure automatic start on reboot
+include_recipe 'powershell::powershell5'
+
+dsc_script 'Install-SNMP-Service' do
+	code <<-EOH
+  WindowsFeature SnmpService
+  {
+    Ensure = 'Present'
+    Name = 'SNMP-Service'
+  }
+  EOH
+  timeout 600
+end
+
+dsc_script 'Install-SNMP-WMI-Provider' do
+  code <<-EOH
+  WindowsFeature SnmpService
+  {
+    Ensure = 'Present'
+    Name = 'SNMP-WMI-Provider'
+  }
+  EOH
+  timeout 600
+end
+
 windows_service 'SNMP' do
 	action [:enable, :start]
 end
